96c2966c0aed948de85b985bf38c22bdfb3dcfea
namlh201 <namlh201@gmail.com>
2024-09-08 19:36:27 +0200

    Fix bug

--- test_seamless_m4t.py
+++ test_seamless_m4t.py
@@ -41,7 +41,7 @@ def generate_one(
     seamless_processor: SeamlessM4TProcessor,
     audio_feats: torch.Tensor
 ) -> tuple[list[str], list[str]]:
-    translated_tokens = seamless_model.generate(audio_feats, tgt_lang='deu', generate_speech=False)
+    translated_tokens = seamless_model.generate(audio_feats, tgt_lang='deu')
 
     translations = seamless_processor.decode(translated_tokens[0].tolist()[0], skip_special_tokens=True)
 
